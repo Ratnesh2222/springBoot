9329863467d3920a125e32098f565502641ab8ff
Ratnesh Srivastava <Ratnesh.Srivastava@ibm.com>
2024-09-18 10:43:45 +0530

    test

--- myspringboot/src/main/java/com/self/myspringboot/service/ServiceImplTwo.java
+++ myspringboot/src/main/java/com/self/myspringboot/service/ServiceImplTwo.java
@@ -1,8 +1,10 @@
 package com.self.myspringboot.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class ServiceImplTwo implements Service{
 
     public String showData() {
