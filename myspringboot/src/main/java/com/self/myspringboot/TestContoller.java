2b44021e7596664fb10d5305900a02e8606e0ef9
Ratnesh Srivastava <Ratnesh.Srivastava@ibm.com>
2024-09-18 08:51:23 +0530

    springBoot project

---
+++ myspringboot/src/main/java/com/self/myspringboot/TestContoller.java
@@ -0,0 +1,14 @@
+package com.self.myspringboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestContoller {
+
+    @GetMapping("/start")
+    public String start() {
+        return "return from spring boot on aws";
+    }
+
+}
