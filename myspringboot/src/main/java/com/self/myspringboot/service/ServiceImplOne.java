9c0b8fdcbae824df3f5dd2b681d8b9a9b23f346b
Ratnesh Srivastava <Ratnesh.Srivastava@ibm.com>
2024-09-18 10:39:35 +0530

    test

--- myspringboot/src/main/java/com/self/myspringboot/service/ServiceImplOne.java
+++ myspringboot/src/main/java/com/self/myspringboot/service/ServiceImplOne.java
@@ -1,11 +1,9 @@
 package com.self.myspringboot.service;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
-public class ServiceImplOne implements service{
+public class ServiceImplOne implements Service{
 
     public String showData() {
         return "return showData from ServiceImplOne, impl service";
